2520aa210da199d4975a29d4dc7664c6bd7d1542
jeonghan <kimjh9813@naver.com>
2022-07-05 22:11:43 +0900

    FIX(gazeobo): Change version - noetic
    change version melodic to noetic and excute gazebo launch

--- hongdo_ros_teleop/setup.py
+++ hongdo_ros_teleop/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['hongdo_ros_bringup'],
+    packages=['hongdo_ros_teleop'],
     package_dir={'': 'src'}
 )
 
